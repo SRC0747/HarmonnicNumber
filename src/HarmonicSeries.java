bce11a69f652da4492aba03ee0bbcd5ae15ecdb2
SRC0747 <srcsrk1998@gmail.com>
2021-08-08 14:35:55 +0530

    Calculating Harmonic series.

---
+++ src/HarmonicSeries.java
@@ -0,0 +1,13 @@
+public class HarmonicSeries {
+    public static void main(String[] args)
+    {
+        int n=7;
+        int hp=0;
+        int num;
+        for (num=1; num<n; num++)
+        {
+            hp = hp+(1/num);
+        }
+        System.out.println(hp);
+    }
+}
